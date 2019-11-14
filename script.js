a8e4a9026939a511b4c294cd519345faacd593b8
Emerson Alves Lima <emersonsintemas@gmail.com>
2019-11-14 16:41:57 -0300

    add variaveis and if/else

---
+++ script.js
@@ -0,0 +1,12 @@
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let ta = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        window.alert('Por favor digite um número.')  
+    } else {
+        let n = Number(num.value)
+    }
+    
+
+}
