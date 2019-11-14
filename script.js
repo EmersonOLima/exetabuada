73b4bafb89b7c09457485b88cf3b8835c9a7ee8f
Emerson Alves Lima <emersonsintemas@gmail.com>
2019-11-14 17:01:20 -0300

    mudanças pequenas

--- script.js
+++ script.js
@@ -7,9 +7,13 @@ function tabuada() {
     } else {
         let n = Number(num.value)
         let c = 1
+        ta.innerHTML = ' '
         while (c <= 10) {
             let item = document.createElement('option')//estou criando um elemnto option
-            
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            ta.appenChild(item)//add um elemento filho
+            c++
         }
     }
     
